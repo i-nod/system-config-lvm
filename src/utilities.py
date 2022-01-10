d77f42eda5fed1115d9a4512c502e061e1988219
i-nod <nodes@gm.com>
2022-01-09 21:54:13 -0800

    added some ubuntu compatibility by allowing readlink to be in /bin instead of /usr/bin. Still need to fix how make install runs since it doesn't seem to work properly for ubuntu

--- src/utilities.py
+++ src/utilities.py
@@ -1,4 +1,4 @@
-
+import os
 from execute import execWithCapture, execWithCaptureErrorStatus, execWithCaptureStatus
 
 cache_readlink_o = {}
@@ -10,15 +10,27 @@ def follow_links_to_target(path, paths=[]):
     
     if path.startswith("/") == False:
         return None
-    
-    if path not in cache_readlink_s:
-      o, s = execWithCaptureStatus('/usr/bin/readlink', ['/usr/bin/readlink', '-e', path])
-      cache_readlink_o[path] = o
-      cache_readlink_s[path] = s
-    else:
-      s = cache_readlink_s[path]
-      o = cache_readlink_o[path]
 
+    #ubuntu puts readlink in /bin so need to see if it exists in /usr/bin or /bin
+    if os.path.isfile("/usr/bin/readlink"):
+        if path not in cache_readlink_s:
+            o, s = execWithCaptureStatus('/usr/bin/readlink', ['/usr/bin/readlink', '-e', path])
+            cache_readlink_o[path] = o
+            cache_readlink_s[path] = s
+        else:
+            s = cache_readlink_s[path]
+            o = cache_readlink_o[path]
+    elif os.path.isfile("/bin/readlink"):
+        if path not in cache_readlink_s:
+            o, s = execWithCaptureStatus('/bin/readlink', ['/bin/readlink', '-e', path])
+            cache_readlink_o[path] = o
+            cache_readlink_s[path] = s
+        else:
+            s = cache_readlink_s[path]
+            o = cache_readlink_o[path]
+    else: #can't find readlink
+        return None
+    
     if s == 0:
         word = o.strip()
         
